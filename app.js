86535f194a929c94c36d8d1d6de8e5f5a840543c
Duurenbayar <89.duuree@hmail.com>
2021-07-02 00:50:08 +0800

    init function holboson

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 // Delgetstei ajillah controller
-var uiController = (function() {})();
+var uiController = (function() {
+
+    var DOMstrings = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add_value",
+        addBtn: ".add__btn"
+    };
+
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
+})();
 
 // Sanhuutei ajillah controller
 var financeController = (function() {})();
@@ -14,15 +36,29 @@ var appController = (function(uiController, financeController) {
         // 3. Olj avsan ugugdluudee web deeree tohiroh hesegt ni gargana.
         // 4. Tusviig tootsoolno
         // 5. Etssiin uldegdel, tootsoog delgetsend gargana.
-    }
-    document.querySelector('.add__btn'),addEventListener('click', function() {
-        ctrlAddItem();
-    });
+    };
+    
+    var setupEventListeners = function() {
 
-    document.addEventListener("keypress", function(event) {
-        if (event.keyCode === 13 || event.which === 13) {
+        var DOM = uiController.getDOMstrings();
+
+        document.querySelector(DOM.addBtn),addEventListener('click', function() {
             ctrlAddItem();
-        }
-    });
+        });
+    
+        document.addEventListener("keypress", function(event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
 
+        return {
+            init: function() {
+                console.log("Application started...");
+                setupEventListeners();
+            }
+        };
+    };
 })(uiController, financeController); 
+
+appController.init();
